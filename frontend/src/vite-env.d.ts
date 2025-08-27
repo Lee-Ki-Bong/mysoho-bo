6a392fb80ee9f1b13d065eb68b9af7657f28c33b
이기봉 <whitebong@igibong-ui-MacBookAir.local>
2025-08-27 14:55:12 +0900

    chore(front): env 초안

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+  // 다른 키도 여기서 추가
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
