49d4ac32f4f0715acc4028df18d5bb55b92e0dff
이기봉 <whitebong@igibong-ui-MacBookAir.local>
2025-08-27 15:59:45 +0900

    fix(front): 소스 정리

--- frontend/src/features/sidebar/nav-user.tsx
+++ frontend/src/features/sidebar/nav-user.tsx
@@ -1,10 +1,9 @@
-import { BadgeCheck, Bell, ChevronsUpDown, CreditCard, LogIn, LogOut, Sparkles } from 'lucide-react'
+import { ChevronsUpDown,  LogIn, LogOut } from 'lucide-react'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
